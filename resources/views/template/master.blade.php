0d288e77dbbd76663259327c32049df8cc12938e
MrRo6ot <esramtin51@gmail.com>
2020-06-26 03:04:32 +0430

    ramtin commit

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
-<html lang="en-US">
+    <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta name="keywords" content="Gambling, Bet, Lucky, Free Btc, Free Crypto, Dice, Crash">
+        <meta name="keywords"
+              content="Gambling, Bet, Lucky, Free Btc,
+                       Free Crypto, Dice, Crash">
         <meta name="viewport"
               content="width=device-width, user-scalable=no,
-                            initial-scale=1.0, maximum-scale=1.0,
-                            minimum-scale=1.0">
+                       initial-scale=1.0, maximum-scale=1.0,
+                       minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>MrRo6ot</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        <link rel="stylesheet"
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
               integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
               crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
     <body>
-        <div class="container-flueid">
+        <div class="container-flueid"> <!-- this tag is a content body -->
             @include('template.navbar')
             <main class="container-flueid">
                 <div class="d-flex flex-row">
